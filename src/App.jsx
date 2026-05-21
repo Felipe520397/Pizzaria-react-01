4753c9af12aba64227ee542bd2f67af728146ec5
Felipe520397 <epilef1357908642@gmail.com>
2026-05-21 12:03:35 -0300

    trabalho-andre

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  const pizzas = [
+    {
+      id: 1,
+      nome: "Pizza Calabresa",
+      descricao: "Pizza com calabresa, cebola e queijo.",
+      imagem: "https://images.unsplash.com/photo-1513104890138-7c749659a591?q=80&w=1000",
+      preco: 40
+    },
+
+    {
+      id: 2,
+      nome: "Pizza Frango",
+      descricao: "Pizza de frango com catupiry.",
+      imagem: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=1000",
+      preco: 45
+    },
+
+    {
+      id: 3,
+      nome: "Pizza Portuguesa",
+      descricao: "Pizza com presunto, ovo e queijo.",
+      imagem: "https://images.unsplash.com/photo-1574071318508-1cdbab80d002?q=80&w=1000",
+      preco: 50
+    }
+  ]
+
+  const [cart, setCart] = React.useState([])
+
+  function adicionarPizza(pizza) {
+    setCart([...cart, pizza])
+  }
+
+  function removerPizza(index) {
+    const novoCarrinho = cart.filter((_, i) => i !== index)
+    setCart(novoCarrinho)
+  }
+
+  const total = cart.reduce((acc, item) => acc + item.preco, 0)
+  return (
+    <>
+      <header>
+        <h1>Pizzaria</h1>
+
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Menu</a></li>
+            <li><a href="#">Contato</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+
+        <section>
+
+          <h2>Menu</h2>
+
+{
+  pizzas.map((pizza) => (
+
+    <article key={pizza.id}>
+
+      <h3>{pizza.nome}</h3>
+
+      <img
+        src={pizza.imagem}
+        alt={pizza.nome}
+      />
+
+      <p>{pizza.descricao}</p>
+
+      <p>R$ {pizza.preco}</p>
+
+      <button onClick={() => adicionarPizza(pizza)}>
+        Order
+      </button>
+
+    </article>
+
+  ))
+}
+
+        </section>
+        <section>
+
+  <h2>Carrinho</h2>
+
+  {
+    cart.length === 0
+    ? <p>Carrinho vazio</p>
+
+    : cart.map((item, index) => (
+
+      <article key={index}>
+
+        <h3>{item.nome}</h3>
+
+        <p>R$ {item.preco}</p>
+
+        <button onClick={() => removerPizza(index)}>
+          Remover
+        </button>
+
+      </article>
+
+    ))
+  }
+
+  <h2>Total: R$ {total}</h2>
+
+</section>
+
+      </main>
+
+      <footer>
+        <p>© 2023 Pizzeria Name</p>
+      </footer>
+    </>
+  )
+}
+
+export default App
